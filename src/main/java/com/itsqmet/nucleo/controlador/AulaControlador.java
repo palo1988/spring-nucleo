5c0b5f59b04e664bb189c7ff83d85530d8adaf03
Pablo Rosero <pabblorosero_88@hotmail.com>
2024-09-23 01:50:01 -0500

    entregable 3 parte final

--- src/main/java/com/itsqmet/nucleo/controlador/AulaControlador.java
+++ src/main/java/com/itsqmet/nucleo/controlador/AulaControlador.java
@@ -1,12 +1,57 @@
 package com.itsqmet.nucleo.controlador;
 
+import com.itsqmet.nucleo.modelo.Aula;
+import com.itsqmet.nucleo.servicio.IAulaServicio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/aula")
 public class AulaControlador {
-    @GetMapping("/aula")
-    public String paginaAula(){
-        return "/aula/listarAula";
+
+    @Autowired
+    private IAulaServicio aulaServicio;
+
+    @GetMapping("/list")
+    public String listarAulas(Model model) {
+        List<Aula> aulas = aulaServicio.listarAulas();
+        model.addAttribute("aulas", aulas);
+        return "aula/aula-list";
+    }
+
+    @GetMapping("/create")
+    public String mostrarFormularioCreacion(Model model) {
+        model.addAttribute("aula", new Aula());
+        return "aula/aula-create";
+    }
+
+    @PostMapping("/save")
+    public String guardarAula(@ModelAttribute Aula aula) {
+        aulaServicio.insertarAula(aula);
+        return "redirect:/aula/list";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String mostrarFormularioEdicion(@PathVariable("id") Integer id, Model model) {
+        Aula aula = aulaServicio.obtenerAulaPorId(id);
+        model.addAttribute("aula", aula);
+        return "aula/aula-edit";
+    }
+
+    @PostMapping("/update/{id}")
+    public String actualizarAula(@PathVariable("id") Integer id, @ModelAttribute Aula aula) {
+        aula.setIdAula(id);
+        aulaServicio.actualizarAula(aula);
+        return "redirect:/aula/list";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String eliminarAula(@PathVariable("id") Integer id) {
+        aulaServicio.eliminarAula(id);
+        return "redirect:/aula/list";
     }
 }
--- src/main/java/com/itsqmet/nucleo/controlador/ProfesorControlador.java
+++ src/main/java/com/itsqmet/nucleo/controlador/ProfesorControlador.java
@@ -1,4 +1,57 @@
 package com.itsqmet.nucleo.controlador;
 
+import com.itsqmet.nucleo.modelo.Profesor;
+import com.itsqmet.nucleo.servicio.IProfesorServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@Controller
+@RequestMapping("/profesor")
+
 public class ProfesorControlador {
+
+    @Autowired
+    private IProfesorServicio profesorServicio;
+
+    @GetMapping("/list")
+    public String listarProfesores(Model model) {
+        List<Profesor> profesores = profesorServicio.listarProfesores();
+        model.addAttribute("profesores", profesores);
+        return "profesor/profesor-list";
+    }
+
+    @GetMapping("/create")
+    public String mostrarFormularioCreacion(Model model) {
+        model.addAttribute("profesor", new Profesor());
+        return "profesor/profesor-create";
+    }
+
+    @PostMapping("/save")
+    public String guardarProfesor(@ModelAttribute Profesor profesor) {
+        profesorServicio.insertarProfesor(profesor);
+        return "redirect:/profesor/list";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String mostrarFormularioEdicion(@PathVariable("id") Integer id, Model model) {
+        Profesor profesor = profesorServicio.obtenerProfesorPorId(id);
+        model.addAttribute("profesor", profesor);
+        return "profesor/profesor-edit";
+    }
+
+    @PostMapping("/update/{id}")
+    public String actualizarProfesor(@PathVariable("id") Integer id, @ModelAttribute Profesor profesor) {
+        profesor.setIdProfesor(id);
+        profesorServicio.actualizarProfesor(profesor);
+        return "redirect:/profesor/list";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String eliminarProfesor(@PathVariable("id") Integer id) {
+        profesorServicio.eliminarProfesor(id);
+        return "redirect:/profesor/list";
+    }
 }
